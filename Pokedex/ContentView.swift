6d88c611fd7215007ae2b76c5db3bcc5fedd21d3
Cristian Cojocaru <cristiancojocarudev@gmail.com>
2024-02-06 15:18:33 +0100

    Header

--- Pokedex/ContentView.swift
+++ Pokedex/ContentView.swift
@@ -9,12 +9,35 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        GeometryReader { geo in
-            Image(.pokewallpaper)
-                .resizable()
-                .scaledToFill()
-                .frame(width: geo.size.width, height: geo.size.height * 1.1, alignment: .center)
-                .ignoresSafeArea()
+        ZStack {
+            GeometryReader { geo in
+                Image(.pokewallpaper)
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: geo.size.width, height: geo.size.height * 1.1, alignment: .center)
+                    .ignoresSafeArea()
+            }
+            
+            GeometryReader { geo in
+                VStack {
+                    HStack {
+                        Text("Pokedex")
+                            .font(.largeTitle)
+                            .foregroundStyle(.red)
+                            .padding(.leading)
+                            .shadow(radius: 3)
+                        Image(.pokedexImg)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(height: geo.size.height * 0.1)
+                    }
+                    .background(.white)
+                    .cornerRadius(15)
+                    .shadow(color: .red, radius: 5, x: geo.size.width * 0.03, y: geo.size.width * 0.03)
+                    .shadow(color: .blue, radius: 5, x: -geo.size.width * 0.03, y: -geo.size.width * 0.03)
+                }
+                .padding()
+            }
         }
     }
 }
