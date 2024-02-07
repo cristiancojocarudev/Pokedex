85b61346225554f1149ade243f5bd8bff730a5ba
Cristian Cojocaru <cristiancojocarudev@gmail.com>
2024-02-07 17:46:06 +0100

    Details header added

--- Pokedex/DetailsPage/View/DetailsView.swift
+++ Pokedex/DetailsPage/View/DetailsView.swift
@@ -10,8 +10,60 @@ import SwiftUI
 struct DetailsView: View {
     @ObservedObject var detailsViewModel: DetailsViewModel
     
+    var details: PokemonDetails {
+        get {
+            detailsViewModel.pokemonDetails
+        }
+    }
+    
     var body: some View {
-        Text(detailsViewModel.pokemonDetails.name)
+        ZStack {
+            GeometryReader { geo in
+                HStack {
+                    ZStack {
+                        HStack {
+                            Spacer()
+                            AsyncImage(url: URL(string: details.sprites.other.home.front_default)) { image in
+                                image
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: geo.size.width / 2, height: geo.size.height * 0.2)
+                                    .padding(.top, geo.size.height * 0.03)
+                                    .ignoresSafeArea()
+                                    .shadow(color: .red, radius: 5, x: geo.size.width * 0.03, y: geo.size.width * 0.03)
+                                    .shadow(color: .blue, radius: 5, x: -geo.size.width * 0.03, y: -geo.size.width * 0.03)
+                                    .padding()
+                            } placeholder: {
+                                ProgressView()
+                            }
+                        }
+                        
+                        HStack {
+                            Text(details.name)
+                                .font(.title)
+                                .padding()
+                                .background(.white)
+                                .cornerRadius(15)
+                                .overlay(
+                                    RoundedRectangle(cornerRadius: 15)
+                                        .stroke(.blue, lineWidth: 5)
+                                )
+                        }
+                        .frame(width: geo.size.width, height: geo.size.height * 0.25, alignment: .bottomLeading)
+                        .padding(.leading, geo.size.width * 0.2)
+                        .padding(.bottom, geo.size.height * 0.05)
+                           
+                    }
+                }
+                .frame(width: geo.size.width, height: geo.size.height * 0.25)
+                .cornerRadius(36)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 36)
+                        .stroke(.red, lineWidth: 5)
+                )
+                .ignoresSafeArea()
+            }
+        }
     }
 }
 
@@ -48,7 +100,7 @@ struct DetailsView_Preview: PreviewProvider {
                 PokeTypeWrapper(type: PokeType(name: "grass")),
                 PokeTypeWrapper(type: PokeType(name: "poison"))
             ]
-            let sprites: SpritesWrapper = SpritesWrapper(back_default: "https://raw.githubuserco…rites/pokemon/back/1.png", front_default: "https://raw.githubuserco…er/sprites/pokemon/1.png", other: OtherSpritesWrapper(dream_world: FrontSprite(front_default: "https://raw.githubuserco…/other/dream-world/1.svg"), home: FrontSprite(front_default: "https://raw.githubuserco…pokemon/other/home/1.png"), showdown: FrontAndBackSprite(front_default: "https://raw.githubuserco…mon/other/showdown/1.gif", back_default: "https://raw.githubuserco…ther/showdown/back/1.gif")))
+            let sprites: SpritesWrapper = SpritesWrapper(back_default: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/1.png", front_default: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png", other: OtherSpritesWrapper(dream_world: FrontSprite(front_default: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"), home: FrontSprite(front_default: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/1.png"), showdown: FrontAndBackSprite(front_default: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/showdown/1.gif", back_default: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/showdown/back/1.gif")))
             let weight: Int = 69
             return PokemonDetails(abilities: abilities, base_experience: base_experience, forms: forms, game_indices: game_indices, height: height, moves: moves, name: name, species: specie, stats: stats, types: types, weight: weight, sprites: sprites)
         }
