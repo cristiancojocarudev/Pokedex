8abaec5b1b22263c81ab322ed537cd20e82106e9
Cristian Cojocaru <cristiancojocarudev@gmail.com>
2024-02-08 18:58:54 +0100

    Home page thumbnails resized

--- Pokedex/HomePage/View/HomeView.swift
+++ Pokedex/HomePage/View/HomeView.swift
@@ -55,13 +55,28 @@ struct HomeView: View {
                                                 DetailsView(detailsViewModel: detailsViewModel)
                                             } label: {
                                                 HStack {
-                                                    if let image = pokemonItem.details.sprites.front_default {
-                                                        AsyncImage(url: URL(string: image))
-                                                            .frame(width: geo.size.width * 0.02, height: geo.size.width * 0.02)
-                                                            .padding()
+                                                    ZStack {
+                                                        HStack {
+                                                            if let imageUrl = pokemonItem.details.sprites.front_default {
+                                                                AsyncImage(url: URL(string: imageUrl)) { image in
+                                                                    image
+                                                                        .resizable()
+                                                                        .scaledToFit()
+                                                                        .frame(width: geo.size.width * 0.25, height: geo.size.width * 0.25)
+                                                                        .padding(.leading)
+                                                                } placeholder: {
+                                                                    ProgressView()
+                                                                }
+                                                            }
+                                                            Spacer()
+                                                        }
+                                                        HStack {
+                                                            Spacer()
+                                                            Text(pokemonItem.reference.name)
+                                                                .padding(.leading, geo.size.width * 0.07)
+                                                            Spacer()
+                                                        }
                                                     }
-                                                    Text(pokemonItem.reference.name)
-                                                        .padding()
                                                 }
                                                 .frame(width: geo.size.width * 0.8, height: geo.size.height * 0.1)
                                                 .background(.white)
